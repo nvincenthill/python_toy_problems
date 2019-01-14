61eec9883d1821f22aa84558dd28ba44dab068f7
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-14 10:32:22 -0800

    Add binary_add problem

---
+++ binary_add.py
@@ -0,0 +1,15 @@
+# Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+# The binary number returned should be a string.
+
+
+def add_binary(a, b):
+    # your code here
+    pass
+
+
+# print(add_binary(1, 1) == "10")
+# print(add_binary(0, 1) == "1")
+# print(add_binary(1, 0) == "1")
+# print(add_binary(2, 2) == "100")
+# print(add_binary(51, 12) == "111111")
