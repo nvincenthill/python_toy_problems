6579d5544aeb2c1413a85d7620735b88932bb340
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-14 10:40:05 -0800

    Implement binary_add

--- binary_add.py
+++ binary_add.py
@@ -4,9 +4,33 @@
 
 
 def add_binary(a, b):
-    # your code here
-    pass
+    x = format(a, "b")
+    y = format(b, "b")
+    max_len = max(len(x), len(y))
 
+    x = x.zfill(max_len)
+    y = y.zfill(max_len)
+
+    # initialize the result
+    result = ''
+
+    # initialize the carry
+    carry = 0
+
+    # traverse the string
+    for i in range(max_len - 1, -1, -1):
+        r = carry
+        r += 1 if x[i] == '1' else 0
+        r += 1 if y[i] == '1' else 0
+        result = ('1' if r % 2 == 1 else '0') + result
+        carry = 0 if r < 2 else 1     # compute the carry
+
+    if carry != 0:
+        result = '1' + result
+
+    return result.zfill(max_len)
+
+# tests
 
 # print(add_binary(1, 1) == "10")
 # print(add_binary(0, 1) == "1")
