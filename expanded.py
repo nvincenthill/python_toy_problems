edc45d4d1ffb68bacd0ff80fc46c1ad6a1a5e233
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-23 17:02:22 -0800

    Implement better iterative solution

--- expanded.py
+++ expanded.py
@@ -7,22 +7,26 @@
 # NOTE: All numbers will be whole numbers greater than 0.
 
 
+def round_down(num, divisor):
+    return num - (num % divisor)
+
+
 def expanded_form(num):
     stringified = str(num)
-    length_of_string = len(stringified)
-    # base case
-    if len(stringified) == 1:
-        return stringified
-    # recursive case
-    else:
-        base = int(round(num, (length_of_string - 1) * -1))
-        return str(base) + " + " + expanded_form(num - base)
+    result = ''
+
+    while True:
+        stringified = str(num)
+        if len(stringified) == 1:
+            return result + str(num)
+        else:
+            base = int(round_down(num, 10 ** (len(stringified) - 1)))
+            result = result + str(base) + " + "
+            num = num - base
 
 
 print(expanded_form(3) == '3')
 print(expanded_form(12) == '10 + 2')
 print(expanded_form(42) == '40 + 2')
 print(expanded_form(70304) == '70000 + 300 + 4')
-
-# recursive solution fails due to maximum recursion depth exceeded
-print(expanded_form(4982342))
+# print(expanded_form(4982342))
